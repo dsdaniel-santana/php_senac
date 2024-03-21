3b402aae2b37bcc9e0035823b9b96f10868480ab
daniel <daniel_santana_87@hotmail.com>
2024-03-20 22:15:41 -0300

    criando arquivo PHP, variaveis

---
+++ 01/teste.php
@@ -0,0 +1,41 @@
+<?php
+
+echo "Helo World<br>";  
+  
+
+// Isso é um comentário
+# isso também é um comentário
+
+/* 
+Isso é comentário de múltiplas linhas
+eu disse, múltiplas linhas
+*/
+
+// Declaração de variáveis
+$nome = "John";
+$sobrenome = "Smith ";
+$nome_completo = $nome." ".$sobrenome;
+
+
+//echo $nome;
+//echo $sobrenome;
+echo $nome." ".$sobrenome ."<br>";
+echo $nome_completo ."<br>"; 
+
+// Variáveis numéricas
+$idade = 38;
+$ano = 2024;
+echo $idade ."<br>";
+echo $ano - $idade ."<br>"; //operações matemáticas
+
+echo 'Esse é um texto em aspas simpes <br>';
+echo "Esse é um texto em aspas duplas <br>";
+
+// Trabalhando com aspas simples e duplas
+echo 'Dom Pedro disse "Independencia ou Morte"<br>';
+
+echo "$nome tem $idade anos de idade <br>";
+echo '$nome tem $idade anos de idade';
+?>
+
+
