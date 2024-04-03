a822b7831b8e84e088a9d90ad05987c969ebd558
dsdaniel-santana <129440429+dsdaniel-santana@users.noreply.github.com>
2024-04-03 11:21:21 -0300

    atualizando

--- 10/exercicio.php
+++ 10/exercicio.php
@@ -6,26 +6,36 @@ class Vehicles {
     public $years;
     public $ligado;
 
-    public function __construct($brands, $models, $years, $areOn) {
+    public function __construct($brands, $models, $years) {
         $this->brands = $brands;
         $this->models = $models;
         $this->years = $years;
         $this->ligado  = false;
     }
 
-    public function ligar($areOn = true){
-        return "o veiculo esta ligado";
+    public function ligar(){
+        $this->ligado == false ? $this->ligado = true : null;
     }
 
-    public function desligar($areOn = false){
-        return "o veiculo esta desligado";
+    public function desligar(){
+        $this->ligado == true ? $this->ligado = false : null;
+    }
+
+    public function exibirDados(){
+        echo "Marca: $this->brands, Modelo:$this->models, Ano: $this->years, Ligado: $this->ligado <br>";
     }
 
 }
 
 $meuCarro = new Vehicles("chevete", "chevrolet", 1986);
-echo "$meuCarro->brands, $meuCarro->models, $meuCarro->years";
 
+$meuCarro->exibirDados();
+$meuCarro->ligar();
+
+$meuCarro->exibirDados();
+$meuCarro->desligar();
+
+$meuCarro->exibirDados();
 
 /*
 1 - Defina uma classe chamada Veiculo com as seguintes propriedades:
@@ -42,7 +52,6 @@ propriedade "ligado" como false por padrão.
  se o veículo estiver desligado.
 - desligar(): Este método deve alterar a propriedade "ligado" para false, se o veículo estiver ligado.
 - exibirDados(): Este método deve exibir as informações do veículo(marca, modelo, ano, e status de ligado ou desligado)
-*/
 
 // Exibindo dados do veículo
 $meuCarro->exibirDados(); // Marca: Toyota, Modelo: Corolla, Ano: 2022, Ligado: false
@@ -58,5 +67,9 @@ $meuCarro->ligar();
 
 // Exibindo dados atualizados
 $meuCarro->exibirDados(); // Marca: Toyota, Modelo: Corolla, Ano: 2022, Ligado: false
+
+*/
+
+
 
 ?>
