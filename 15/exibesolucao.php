d1c03abc8c2b91dd56dafcb5747c499cb1c440cf
daniel <daniel_santana_87@hotmail.com>
2024-04-11 22:08:24 -0300

    aula 15 finalizando  exercicos

---
+++ 15/exibesolucao.php
@@ -0,0 +1,13 @@
+<?php
+require_once 'solucao.php';
+$repository = new DatabaseRepository();
+
+//print_r($repository->getAllItems());
+
+//echo $repository->addItem("Abacaxi", 2);
+
+//$repository->updateItem(4, "Arroz", 1, 1);
+
+//$repository->deleteItem(4);
+
+?>
