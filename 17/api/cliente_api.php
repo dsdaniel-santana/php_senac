6b3127bc53ec3da8f98d25835cd04ab0fe6432d7
daniel <daniel_santana_87@hotmail.com>
2024-04-18 22:06:30 -0300

    aula 17 Cliente repository completo

--- 17/api/cliente_api.php
+++ 17/api/cliente_api.php
@@ -8,6 +8,19 @@ switch($action) {
     case 'listar':
         echo json_encode(ClienteRepository::getAllClientes());
         break;
+
+    case 'buscar':
+        buscarClinteID();
+        break;
+    case 'cadastrar':
+        cadastrarCliente();
+    break;
+    case 'atualizar':
+        atualizarCliente();
+    case 'excluir':
+        excluirCliente();
+    break;
+       
     default:
         http_response_code(400); // Requisição inválida
         echo json_encode(['error' => 'Ação inválida']);
@@ -17,3 +30,21 @@ function listarClientes(){
     $clientes = ClienteRepository::getAllClientes();
     echo json_encode($clientes);
 }
+
+function buscarClinteID(){
+    $id = $_GET['id'];
+
+}
+
+function cadastrarCliente(){
+
+}
+
+function atualizarCliente(){
+
+}
+
+function excluirCliente(){
+
+}
+
