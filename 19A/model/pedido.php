35d6b50bd88a6c498f03e9df772f69d9407b229c
daniel <daniel_santana_87@hotmail.com>
2024-05-08 21:39:32 -0300

    pedido.php

---
+++ 19A/model/pedido.php
@@ -0,0 +1,42 @@
+<?php
+
+class pedido implements JsonSerializable{
+    private $id;
+    private $data_pedido;
+    private $status;
+
+    public function __construct($id, $data_pedido, $status)
+    {
+        $this->id = $id;
+        $this-> data_pedido = $data_pedido;
+        $this->status = $status;
+    }
+
+    public function getDataPedido(){
+        return $this->data_pedido;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+
+    public function setData($data){
+        $this->data_pedido = $data;
+    }
+
+    public function setStatus($status){
+        $this->status = $status;
+    }
+
+    public function jsonSerialize(): array {
+        return [
+            'id' => $this->id,
+            'data_pedido' => $this->data_pedido,
+            'status' => $this->status
+        ];
+        
+    }
+}
+
+
+?>
